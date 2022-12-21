0ef1d506aab42b0d19d8cab566b41ec659f3ac18
Yon Bollain <bollainyon@gmail.com>
2022-12-21 12:04:27 +0100

    cambios

---
+++ js/home.js
@@ -0,0 +1,9 @@
+function main(){
+document.querySelectorAll('*').forEach(el => {
+    console.log("pase");
+    if (el.offsetWidth > document.documentElement.offsetWidth) {
+        console.log('Element que desquadra: ', el);
+    }
+  });
+}
+document.addEventListener("DOMContentLoaded",main);
