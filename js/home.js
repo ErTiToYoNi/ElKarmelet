26174673a7d2c87e41fd5ff2c4b4c7a945e145c1
Yon <bollainyon@gmail.com>
2022-12-23 17:22:49 +0100

    Cambios

--- js/home.js
+++ js/home.js
@@ -1,9 +1,10 @@
 function main(){
-document.querySelectorAll('*').forEach(el => {
+  document.querySelectorAll('*').forEach(el => {
     console.log("pase");
     if (el.offsetWidth > document.documentElement.offsetWidth) {
         console.log('Element que desquadra: ', el);
     }
   });
+  
 }
 document.addEventListener("DOMContentLoaded",main);
