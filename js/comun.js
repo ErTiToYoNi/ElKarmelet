353f6f12f43254628f2a5ceaf4a1b288493ba9f2
Yon <bollainyon@gmail.com>
2023-01-02 10:38:53 +0100

    responsive menu y footer

--- js/comun.js
+++ js/comun.js
@@ -1,7 +1,19 @@
 function main(){
+    document.querySelector(".menu-1").style.display = "none";
+    document.getElementById('menu').addEventListener("click",mostrar);
     document.querySelector(".reservar").addEventListener("click",redirect,false);
 }
 function redirect(){
     window.location.href = 'reservas.html';
+}
+function mostrar(){
+    let menu = document.querySelector(".menu-1");
+    if(menu.style.display !='none'){
+        menu.style.display = 'none';
+    }else{
+        console.log("hola")
+        menu.style.display = 'block'
+        console.log(menu);
+    }
 }
 document.addEventListener("DOMContentLoaded",main);
