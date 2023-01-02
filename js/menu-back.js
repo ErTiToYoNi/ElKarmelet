c8f30bc6ca2544d7f34e0cc8a37a2efb26b2eeae
Yon <bollainyon@gmail.com>
2023-01-02 19:23:51 +0100

    cambios arreglos

--- js/menu-back.js
+++ js/menu-back.js
@@ -1,11 +1,17 @@
 function main(){
     document.querySelector(".flecha-cerrar").addEventListener("click",ocultarmenu,false)
     document.querySelector(".flecha-abrir").addEventListener("click",mostrarmenu,false)
+    document.querySelector('.boton_volver').addEventListener("click",()=>{
+        window.location.href = 'index.html';
+    })
 }
 function ocultarmenu(){
     let menu = document.getElementById("menu").style.display = "none";
 }
 function  mostrarmenu(){
     let menu = document.getElementById("menu").style.display = "block";
+}
+function redirect(){
+
 }
 document.addEventListener("DOMContentLoaded",main)
